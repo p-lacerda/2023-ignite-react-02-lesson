50ac9a7f664d63ed0d50d76b3e56274891e93b78
Paulo Lacerda <paulodetasso@proton.me>
2023-05-11 11:11:17 -0300

    feature: implements Header component

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  background: ${props => props.theme["gray-900"]};
+  padding: 2.5rem 0 7.5rem;
+`
+
+export const HeaderContent = styled.header`
+  width: 100%;
+  max-width: 1120px;
+  margin: 0 auto;
+  padding: 0 1.5rem;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const NewTransactionButton = styled.header`
+  display: flex;
+  align-items: center;
+  height: 50px;
+  border: 0;
+  background: ${props => props.theme["green-500"]};
+  font-weight: bold;
+  padding: 0 1.25rem;
+  border-radius: 6px;
+  cursor: pointer;
+  text-align: center;
+
+  &:hover {
+    background: ${props => props.theme["green-700"]};
+    transition: background-color 0.2s;
+  }
+`
